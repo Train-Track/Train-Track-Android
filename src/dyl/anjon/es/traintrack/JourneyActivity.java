94d9cd7962f15b4c748dad3babf82cc5b4117c39
Dylan Jones <dyl@anjon.es>
2014-03-22 00:43:40 +0000

    adds delete journey button to journey

--- src/dyl/anjon/es/traintrack/JourneyActivity.java
+++ src/dyl/anjon/es/traintrack/JourneyActivity.java
@@ -4,16 +4,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 import dyl.anjon.es.traintrack.adapters.JourneyLegRowAdapter;
 import dyl.anjon.es.traintrack.models.Journey;
 import dyl.anjon.es.traintrack.models.JourneyLeg;
 
 public class JourneyActivity extends Activity {
+
+	private int journeyId = 0;
+	private Journey journey;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +27,9 @@ public class JourneyActivity extends Activity {
 		setContentView(R.layout.activity_journey);
 
 		final Intent intent = getIntent();
-		final int journeyId = intent.getIntExtra("journey_id", 0);
+		journeyId = intent.getIntExtra("journey_id", 0);
 
-		Journey journey = Journey.get(this, journeyId);
+		journey = Journey.get(this, journeyId);
 		if (journey == null) {
 			finish();
 		}
@@ -50,6 +56,32 @@ public class JourneyActivity extends Activity {
 
 		});
 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.journey, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.add_journey_leg:
+			Toast.makeText(getApplicationContext(), "Add journey leg",
+					Toast.LENGTH_LONG).show();
+			return true;
+		case R.id.delete_journey:
+			boolean success = journey.delete(getApplicationContext());
+			if (success) {
+				Toast.makeText(getApplicationContext(), "Journey was deleted",
+						Toast.LENGTH_SHORT).show();
+				finish();
+			}
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 }
