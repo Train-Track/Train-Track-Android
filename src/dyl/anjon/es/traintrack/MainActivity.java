891b528dad73bafa9b19fe0a61ef86035fc3d61b
Dylan Jones <dyl@anjon.es>
2014-10-01 22:37:35 +0100

    removes old test

--- src/dyl/anjon/es/traintrack/MainActivity.java
+++ src/dyl/anjon/es/traintrack/MainActivity.java
@@ -49,8 +49,7 @@ public class MainActivity extends FragmentActivity {
 				getSupportFragmentManager());
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mViewPager.setAdapter(mSectionsPagerAdapter);
-		
-		//new GetServiceRequest().execute("HC65HlW6QW14R/OXoD0scg==");
+
 	}
 
 	@Override
