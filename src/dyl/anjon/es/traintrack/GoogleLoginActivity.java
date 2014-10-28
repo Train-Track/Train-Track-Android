4d4ed2c1abfe109b3232dfa0fa62c00337d038fd
Dylan Jones <dyl@anjon.es>
2014-10-28 20:54:42 +0000

    uses shared prefs to know when to connect to google

--- src/dyl/anjon/es/traintrack/GoogleLoginActivity.java
+++ src/dyl/anjon/es/traintrack/GoogleLoginActivity.java
@@ -2,6 +2,7 @@ package dyl.anjon.es.traintrack;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,7 +12,6 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.games.Games;
-import com.google.android.gms.games.Player;
 import com.google.android.gms.plus.Plus;
 
 import dyl.anjon.es.traintrack.utils.BaseGameUtils;
@@ -31,23 +31,16 @@ public class GoogleLoginActivity extends Activity implements
 		findViewById(R.id.sign_in_button).setOnClickListener(this);
 		findViewById(R.id.sign_out_button).setOnClickListener(this);
 
-		// Create the Google Api Client with access to Plus and Games
 		mGoogleApiClient = new GoogleApiClient.Builder(this)
 				.addConnectionCallbacks(this)
 				.addOnConnectionFailedListener(this).addApi(Plus.API)
 				.addScope(Plus.SCOPE_PLUS_LOGIN).addApi(Games.API)
-				.addScope(Games.SCOPE_GAMES)
-				// add other APIs and scopes here as needed
-				.build();
+				.addScope(Games.SCOPE_GAMES).build();
 	}
 
 	@Override
 	public void onConnectionFailed(ConnectionResult result) {
 		Utils.log("CONNECTION FAILED: " + result.toString());
-		// Attempt to resolve the connection failure using BaseGameUtils.
-		// The R.string.signin_other_error value should reference a generic
-		// error string in your strings.xml file, such as "There was
-		// an issue with sign-in, please try again later."
 		if (!BaseGameUtils.resolveConnectionFailure(this, mGoogleApiClient,
 				result, RC_SIGN_IN, getString(R.string.signin_other_error))) {
 		}
@@ -60,15 +53,23 @@ public class GoogleLoginActivity extends Activity implements
 
 	@Override
 	public void onConnected(Bundle connectionHint) {
-		Utils.log("Connected!");
-
 		findViewById(R.id.sign_in_button).setVisibility(View.GONE);
 		findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+		SharedPreferences prefs = getSharedPreferences("current_user",
+				MODE_PRIVATE);
+		prefs.edit().putBoolean("linked_google_account", true).apply();
+	}
 
-		Player p = Games.Players.getCurrentPlayer(mGoogleApiClient);
-		Utils.log("Player: " + p.getDisplayName());
-		Games.Achievements.unlock(mGoogleApiClient,
-				getString(R.string.achievement_first_train));
+	@Override
+	public void onStart() {
+		super.onStart();
+		SharedPreferences prefs = getSharedPreferences("current_user",
+				MODE_PRIVATE);
+		boolean linkedGoogleAccount = prefs.getBoolean("linked_google_account",
+				false);
+		if (linkedGoogleAccount) {
+			mGoogleApiClient.connect();
+		}
 	}
 
 	protected void onActivityResult(int requestCode, int resultCode,
@@ -78,14 +79,9 @@ public class GoogleLoginActivity extends Activity implements
 			if (resultCode == RESULT_OK) {
 				mGoogleApiClient.connect();
 			} else {
-				// Bring up an error dialog to alert the user that sign-in
-				// failed. The R.string.signin_failure should reference an error
-				// string in your strings.xml file that tells the user they
-				// could not be signed in, such as "Unable to sign in."
 				BaseGameUtils.showActivityResultError(this, requestCode,
 						resultCode, R.string.signin_failure,
 						R.string.signin_other_error);
-				Utils.log("ERROR: " + resultCode);
 			}
 		}
 	}
@@ -93,13 +89,12 @@ public class GoogleLoginActivity extends Activity implements
 	@Override
 	public void onClick(View view) {
 		if (view.getId() == R.id.sign_in_button) {
-			// start the asynchronous sign in flow
 			mGoogleApiClient.connect();
 		} else if (view.getId() == R.id.sign_out_button) {
-			// sign out.
 			Games.signOut(mGoogleApiClient);
-
-			// show sign-in button, hide the sign-out button
+			SharedPreferences prefs = getSharedPreferences("current_user",
+					MODE_PRIVATE);
+			prefs.edit().putBoolean("linked_google_account", false).apply();
 			findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
 			findViewById(R.id.sign_out_button).setVisibility(View.GONE);
 		}
