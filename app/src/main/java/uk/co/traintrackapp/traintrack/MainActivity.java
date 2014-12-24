82c78a664a3889c8db5228e554f979e25953514d
Dylan Jones <dyl@anjon.es>
2014-12-24 00:36:38 +0000

    adds quit confirmation

--- app/src/main/java/uk/co/traintrackapp/traintrack/MainActivity.java
+++ app/src/main/java/uk/co/traintrackapp/traintrack/MainActivity.java
@@ -1,18 +1,19 @@
 package uk.co.traintrackapp.traintrack;
 
+import android.app.AlertDialog;
 import android.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 
 import uk.co.traintrackapp.traintrack.fragment.AccountFragment;
 import uk.co.traintrackapp.traintrack.fragment.DashboardFragment;
-
 import uk.co.traintrackapp.traintrack.fragment.JourneysFragment;
+import uk.co.traintrackapp.traintrack.fragment.NavigationDrawerFragment;
 import uk.co.traintrackapp.traintrack.fragment.SettingsFragment;
 import uk.co.traintrackapp.traintrack.fragment.StationsFragment;
-import uk.co.traintrackapp.traintrack.fragment.NavigationDrawerFragment;
 
 
 public class MainActivity extends ActionBarActivity
@@ -68,6 +69,20 @@ public class MainActivity extends ActionBarActivity
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle(mTitle);
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Quit")
+                .setMessage("Are you sure you want to quit?")
+                .setPositiveButton("Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                finish();
+                            }
+                        }).setNegativeButton("No", null).show();
     }
 
 }
