398c69daf7a39f8f1e6e0f89fe4eaafdece7b5b7
Dylan Jones <dyl@anjon.es>
2015-03-15 20:14:55 +0000

    adds a recycler view with cards for dashbaord
    temporary for now

--- app/src/main/java/uk/co/traintrackapp/traintrack/fragment/DashboardFragment.java
+++ app/src/main/java/uk/co/traintrackapp/traintrack/fragment/DashboardFragment.java
@@ -2,14 +2,21 @@ package uk.co.traintrackapp.traintrack.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import uk.co.traintrackapp.traintrack.R;
+import uk.co.traintrackapp.traintrack.adapter.DashboardItemAdapter;
 
 
 public class DashboardFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public static DashboardFragment newInstance() {
         return new DashboardFragment();
@@ -18,7 +25,22 @@ public class DashboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View v = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        String[] mDataset = { "Hello", "Howdy", "Yep", "lovely", "Hello", "Howdy", "Yep", "lovely","Hello", "Howdy", "Yep", "lovely" };
+        mAdapter = new DashboardItemAdapter(mDataset);
+        mRecyclerView.setAdapter(mAdapter);
+        return v;
     }
 
 }
