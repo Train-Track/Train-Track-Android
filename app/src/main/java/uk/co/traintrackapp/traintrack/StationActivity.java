58ba50d35d582e6f5ee71de6ee625e03156d1f7a
Dylan Jones <dyl@anjon.es>
2015-04-07 15:24:18 +0100

    station fragments now do what they are supposed to

--- app/src/main/java/uk/co/traintrackapp/traintrack/StationActivity.java
+++ app/src/main/java/uk/co/traintrackapp/traintrack/StationActivity.java
@@ -2,8 +2,6 @@ package uk.co.traintrackapp.traintrack;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,21 +11,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.astuetz.PagerSlidingTabStrip;
 
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 
-import uk.co.traintrackapp.traintrack.adapter.ServiceItemRowAdapter;
-import uk.co.traintrackapp.traintrack.api.ServiceItem;
-import uk.co.traintrackapp.traintrack.api.StationBoard;
 import uk.co.traintrackapp.traintrack.fragment.StationArrivalsFragment;
 import uk.co.traintrackapp.traintrack.fragment.StationDeparturesFragment;
 import uk.co.traintrackapp.traintrack.fragment.StationDetailsFragment;
@@ -38,11 +26,7 @@ import uk.co.traintrackapp.traintrack.utils.Utils;
 public class StationActivity extends ActionBarActivity {
 
     private Station station;
-    private ServiceItemRowAdapter adapter;
-    private ArrayList<ServiceItem> serviceItems;
-    private ProgressBar progress;
-    private TextView nrccMessage;
-    static final int TAKE_PHOTO = 12;
+    private String journeyUuid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,61 +34,20 @@ public class StationActivity extends ActionBarActivity {
         setContentView(R.layout.activity_station);
 
         final Intent intent = getIntent();
-        final String journeyUuid = intent.getStringExtra("journey_uuid");
         final String stationUuid = intent.getStringExtra("station_uuid");
         TrainTrack app = (TrainTrack) getApplication();
         station = app.getStation(stationUuid);
+        journeyUuid = intent.getStringExtra("journey_uuid");
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(station.getName());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-
-        // Get the ViewPager and set it's PagerAdapter so that it can display items
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
-
-        // Give the PagerSlidingTabStrip the ViewPager
         PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
-        // Attach the view pager to the tab strip
         tabsStrip.setViewPager(viewPager);
-
-
-/*
-
-
-        progress = (ProgressBar) findViewById(R.id.progress);
-        nrccMessage = (TextView) findViewById(R.id.nrcc_messages);
-        serviceItems = new ArrayList<>();
-        adapter = new ServiceItemRowAdapter(serviceItems, station, this);
-
-        final ListView list = (ListView) findViewById(R.id.list);
-        list.setAdapter(adapter);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> arg0, View view, int index,
-                                    long x) {
-                ServiceItem serviceItem = adapter.getItem(index);
-                Intent intent = new Intent().setClass(getApplicationContext(),
-                        ServiceActivity.class);
-                intent.putExtra("journey_uuid", journeyUuid);
-                intent.putExtra("service_id", serviceItem.getServiceId());
-                intent.putExtra("origin_name", serviceItem.getOrigin().getName());
-                intent.putExtra("station_uuid", station.getUuid());
-                intent.putExtra("station_name", station.getName());
-                intent.putExtra("destination_name", serviceItem.getDestination().getName());
-                intent.putExtra("operator_code", serviceItem.getOperator().getCode());
-                intent.putExtra("operator_name", serviceItem.getOperator().getName());
-                intent.putExtra("platform", serviceItem.getPlatform());
-                intent.putExtra("time", serviceItem.getTime());
-                startActivityForResult(intent, 1);
-            }
-
-        });
-
-        new GetBoardRequest().execute(station.getUuid());
-*/
 
     }
 
@@ -134,7 +77,6 @@ public class StationActivity extends ActionBarActivity {
                 app.getUser().save(this);
                 return true;
             case R.id.refresh:
-                new GetBoardRequest().execute(station.getUuid());
                 return true;
             case R.id.map:
                 Intent intent = new Intent().setClass(this, MapActivity.class);
@@ -151,56 +93,13 @@ public class StationActivity extends ActionBarActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TAKE_PHOTO && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap bmp = (Bitmap) extras.get("data");
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
-            byte[] byteArray = stream.toByteArray();
-            //TODO do something with the bytes
-        } else if (resultCode == Activity.RESULT_OK) {
+        if (resultCode == Activity.RESULT_OK) {
             if (getParent() == null) {
                 setResult(Activity.RESULT_OK);
             } else {
                 getParent().setResult(Activity.RESULT_OK);
             }
             finish();
-        }
-    }
-
-    class GetBoardRequest extends AsyncTask<String, String, StationBoard> {
-
-        @Override
-        protected StationBoard doInBackground(String... uuid) {
-            Utils.log("Getting station board...");
-            if (station.isUnderground()) {
-                return StationBoard.getUnderground(uuid[0]);
-            } else if (station.isNationalRail()) {
-                return StationBoard.getDepartures(uuid[0]);
-            }
-            return new StationBoard();
-        }
-
-        @Override
-        protected void onPostExecute(StationBoard board) {
-            super.onPostExecute(board);
-            Utils.log("Got station board.");
-            progress.setVisibility(View.GONE);
-            serviceItems.clear();
-            if (!station.isUnderground()) {
-                serviceItems.addAll(board.getTrainServices());
-            } else {
-                Utils.log(board.getTubeLines().toString());
-                serviceItems.addAll(board.getTubeLines().get(0).getServices());
-            }
-            adapter.notifyDataSetChanged();
-
-            ArrayList<String> nrccMessages = board.getNrccMessages();
-            if (nrccMessages.size() > 0) {
-                nrccMessage.setVisibility(View.VISIBLE);
-                nrccMessage.setText(nrccMessages.get(0));
-            }
-
         }
     }
 
@@ -213,11 +112,11 @@ public class StationActivity extends ActionBarActivity {
             fragments = new ArrayList<>();
             fragments.add(StationDetailsFragment.newInstance(station.getUuid()));
             if (station.isNationalRail()) {
-                fragments.add(StationDeparturesFragment.newInstance(station.getUuid()));
-                fragments.add(StationArrivalsFragment.newInstance(station.getUuid()));
+                fragments.add(StationDeparturesFragment.newInstance(station.getUuid(), journeyUuid));
+                fragments.add(StationArrivalsFragment.newInstance(station.getUuid(), journeyUuid));
             }
             if (station.isUnderground()) {
-                fragments.add(StationUndergroundFragment.newInstance(station.getUuid()));
+                fragments.add(StationUndergroundFragment.newInstance(station.getUuid(), journeyUuid));
             }
         }
 
--- app/src/main/java/uk/co/traintrackapp/traintrack/fragment/StationArrivalsFragment.java
+++ app/src/main/java/uk/co/traintrackapp/traintrack/fragment/StationArrivalsFragment.java
@@ -1,27 +1,45 @@
 package uk.co.traintrackapp.traintrack.fragment;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import uk.co.traintrackapp.traintrack.R;
+import uk.co.traintrackapp.traintrack.ServiceActivity;
 import uk.co.traintrackapp.traintrack.TrainTrack;
+import uk.co.traintrackapp.traintrack.adapter.ServiceItemRowAdapter;
+import uk.co.traintrackapp.traintrack.api.ServiceItem;
+import uk.co.traintrackapp.traintrack.api.StationBoard;
 import uk.co.traintrackapp.traintrack.model.Station;
 import uk.co.traintrackapp.traintrack.utils.Utils;
 
 public class StationArrivalsFragment extends Fragment {
 
-    private static final String ARGS_UUID = "ARGS_UUID";
-    private String pageTitle;
+    private static final String ARGS_STATION_UUID = "ARGS_STATION_UUID";
+    private static final String ARGS_JOURNEY_UUID = "ARGS_JOURNEY_UUID";
+    private static final String PAGE_TITLE = "Arrivals";
     private Station station;
+    private String journeyUuid;
+    private ServiceItemRowAdapter adapter;
+    private ArrayList<ServiceItem> serviceItems;
+    private ProgressBar progress;
+    private TextView nrccMessage;
 
-    public static Fragment newInstance(String uuid) {
+    public static Fragment newInstance(String stationUuid, String journeyUuid) {
         Bundle args = new Bundle();
-        args.putString(Utils.ARGS_PAGE_TITLE, "Arrivals");
-        args.putString(ARGS_UUID, uuid);
+        args.putString(Utils.ARGS_PAGE_TITLE, PAGE_TITLE);
+        args.putString(ARGS_STATION_UUID, stationUuid);
+        args.putString(ARGS_JOURNEY_UUID, journeyUuid);
         StationArrivalsFragment fragment = new StationArrivalsFragment();
         fragment.setArguments(args);
         return fragment;
@@ -30,20 +48,77 @@ public class StationArrivalsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        pageTitle = getArguments().getString(Utils.ARGS_PAGE_TITLE);
-        String uuid = getArguments().getString(ARGS_UUID);
+        String stationUuid = getArguments().getString(ARGS_STATION_UUID);
+        journeyUuid = getArguments().getString(ARGS_JOURNEY_UUID);
         TrainTrack app = (TrainTrack) getActivity().getApplication();
-        station = app.getStation(uuid);
+        station = app.getStation(stationUuid);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_station_details, container, false);
-        TextView title = (TextView) v.findViewById(R.id.name);
-        title.setText(station.getName());
-        TextView uuid = (TextView) v.findViewById(R.id.uuid);
-        uuid.setText(station.getUuid());
+        View v = inflater.inflate(R.layout.fragment_station_board, container, false);
+
+        progress = (ProgressBar) v.findViewById(R.id.progress);
+        nrccMessage = (TextView) v.findViewById(R.id.nrcc_messages);
+        serviceItems = new ArrayList<>();
+        adapter = new ServiceItemRowAdapter(serviceItems, station, getActivity());
+
+        final ListView list = (ListView) v.findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View view, int index,
+                                    long x) {
+                ServiceItem serviceItem = adapter.getItem(index);
+                Intent intent = new Intent().setClass(getActivity(),
+                        ServiceActivity.class);
+                intent.putExtra("journey_uuid", journeyUuid);
+                intent.putExtra("service_id", serviceItem.getServiceId());
+                intent.putExtra("origin_name", serviceItem.getOrigin().getName());
+                intent.putExtra("station_uuid", station.getUuid());
+                intent.putExtra("station_name", station.getName());
+                intent.putExtra("destination_name", serviceItem.getDestination().getName());
+                intent.putExtra("operator_code", serviceItem.getOperator().getCode());
+                intent.putExtra("operator_name", serviceItem.getOperator().getName());
+                intent.putExtra("platform", serviceItem.getPlatform());
+                intent.putExtra("time", serviceItem.getTime());
+                startActivityForResult(intent, 1);
+            }
+
+        });
+
+        new GetArrivalsBoardRequest().execute(station.getUuid());
+
         return v;
     }
 
+    class GetArrivalsBoardRequest extends AsyncTask<String, String, StationBoard> {
+
+        @Override
+        protected StationBoard doInBackground(String... uuid) {
+            Utils.log("Getting station board...");
+            return StationBoard.getArrivals(uuid[0]);
+        }
+
+        @Override
+        protected void onPostExecute(StationBoard board) {
+            super.onPostExecute(board);
+            Utils.log("Got station board.");
+            progress.setVisibility(View.GONE);
+            serviceItems.clear();
+            if (!station.isUnderground()) {
+                serviceItems.addAll(board.getTrainServices());
+            } else {
+                Utils.log(board.getTubeLines().toString());
+                serviceItems.addAll(board.getTubeLines().get(0).getServices());
+            }
+            adapter.notifyDataSetChanged();
+
+            ArrayList<String> nrccMessages = board.getNrccMessages();
+            if (nrccMessages.size() > 0) {
+                nrccMessage.setVisibility(View.VISIBLE);
+                nrccMessage.setText(nrccMessages.get(0));
+            }
+
+        }
+    }
 }
