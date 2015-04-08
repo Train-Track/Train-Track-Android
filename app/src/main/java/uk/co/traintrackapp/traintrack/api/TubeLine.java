ca95414c8eb6df6b82d1f8a78d528f1055eb7875
Dylan Jones <dyl@anjon.es>
2015-04-08 08:43:11 +0100

    formatting for tube status on dashboard

--- app/src/main/java/uk/co/traintrackapp/traintrack/api/TubeLine.java
+++ app/src/main/java/uk/co/traintrackapp/traintrack/api/TubeLine.java
@@ -17,8 +17,21 @@ public class TubeLine {
     private String backgroundColour;
     private String textColour;
     private ArrayList<ServiceItem> services;
+    private String status;
+
+    public TubeLine() {
+        id = 0;
+        uuid = "";
+        name = "";
+        number = "";
+        backgroundColour = "";
+        textColour = "";
+        services = new ArrayList<>();
+        status = "";
+    }
 
     public TubeLine(JSONObject json) {
+        this();
         try {
             id = json.getInt("id");
             uuid = json.getString("uuid");
@@ -26,13 +39,15 @@ public class TubeLine {
             number = json.getString("number");
             backgroundColour = json.getString("background_colour");
             textColour = json.getString("text_colour");
-            services = new ArrayList<>();
             if (!json.isNull("services")) {
                 JSONArray jsonServices = json.getJSONArray("services");
                 for (int i = 0; i < jsonServices.length(); i++) {
                     ServiceItem item = new ServiceItem(jsonServices.getJSONObject(i));
                     services.add(item);
                 }
+            }
+            if (!json.isNull("status")) {
+                status = json.getString("status");
             }
         } catch (JSONException e) {
             Utils.log(e.getMessage());
@@ -67,6 +82,10 @@ public class TubeLine {
         return services;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
     public String toString() {
         return name;
     }
@@ -75,7 +94,7 @@ public class TubeLine {
      * Get the live status of the underground lines
      * @return the list of tube lines
      */
-    public static ArrayList<TubeLine> getStatus() {
+    public static ArrayList<TubeLine> getStatuses() {
         ArrayList<TubeLine> tubeLines = new ArrayList<>();
         String jsonString = Utils.httpGet(Utils.API_BASE_URL + "/tube");
         try {
--- app/src/main/java/uk/co/traintrackapp/traintrack/fragment/DashboardFragment.java
+++ app/src/main/java/uk/co/traintrackapp/traintrack/fragment/DashboardFragment.java
@@ -5,11 +5,13 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -67,7 +69,7 @@ public class DashboardFragment extends Fragment {
         @Override
         protected ArrayList<TubeLine> doInBackground(String... bob) {
             Utils.log("Getting tube status...");
-            return TubeLine.getStatus();
+            return TubeLine.getStatuses();
         }
 
         @Override
@@ -75,16 +77,35 @@ public class DashboardFragment extends Fragment {
             super.onPostExecute(tubeLines);
             Utils.log("Got tube status.");
 
-            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            params.setMargins(12, 12, 12, 12);
+            FrameLayout.LayoutParams linearLayoutParams = new FrameLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            linearLayoutParams.setMargins(12, 0, 12, 0);
+            TableLayout.LayoutParams textViewParams = new TableLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
+            TableLayout.LayoutParams statusTextParams = new TableLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
 
-            for (TubeLine line : tubeLines) {
-                TextView tubeLine = new TextView(getActivity());
-                tubeLine.setText(line.getName());
-                tubeLine.setTextColor(Color.parseColor("#" + line.getTextColour()));
-                tubeLine.setBackgroundColor(Color.parseColor("#" + line.getBackgroundColour()));
-                tubeLine.setLayoutParams(params);
-                tubeLinesView.addView(tubeLine);
+            for (TubeLine tubeLine : tubeLines) {
+
+                LinearLayout linearLayout = new LinearLayout(getActivity());
+                linearLayout.setLayoutParams(linearLayoutParams);
+                linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+                linearLayout.setBackgroundColor(Color.parseColor("#" + tubeLine.getBackgroundColour()));
+                linearLayout.setPadding(12, 12, 12, 12);
+
+                TextView name = new TextView(getActivity());
+                name.setLayoutParams(textViewParams);
+                name.setText(tubeLine.getName());
+                name.setTextSize(18);
+                name.setTextColor(Color.parseColor("#" + tubeLine.getTextColour()));
+                linearLayout.addView(name);
+
+                TextView status = new TextView(getActivity());
+                status.setLayoutParams(statusTextParams);
+                status.setText(tubeLine.getStatus());
+                status.setTextSize(18);
+                status.setTextColor(Color.parseColor("#" + tubeLine.getTextColour()));
+                status.setGravity(Gravity.END);
+                linearLayout.addView(status);
+
+                tubeLinesView.addView(linearLayout);
             }
         }
 
