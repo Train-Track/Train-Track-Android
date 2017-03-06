3260bd2408a5b5af4357fdc5e5610b8c2aa8f390
Dylan Jones <dyl@anjon.es>
2017-03-06 21:54:44 +0000

    Add new permission check for GPS use

--- app/src/main/java/uk/co/traintrackapp/traintrack/MapActivity.java
+++ app/src/main/java/uk/co/traintrackapp/traintrack/MapActivity.java
@@ -1,13 +1,12 @@
 package uk.co.traintrackapp.traintrack;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,6 +20,9 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import uk.co.traintrackapp.traintrack.api.CallingPoint;
 import uk.co.traintrackapp.traintrack.api.Service;
@@ -47,7 +49,18 @@ public class MapActivity extends Activity {
         map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(54.47942,
                 -4.232974)));
         map.animateCamera(CameraUpdateFactory.zoomTo(5));
-        map.setMyLocationEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        } else {
+            map.setMyLocationEnabled(true);
+        }
         map.setOnMapLoadedCallback(new OnMapLoadedCallback() {
             @Override
             public void onMapLoaded() {
--- app/src/main/java/uk/co/traintrackapp/traintrack/fragment/StationsFragment.java
+++ app/src/main/java/uk/co/traintrackapp/traintrack/fragment/StationsFragment.java
@@ -1,10 +1,13 @@
 package uk.co.traintrackapp.traintrack.fragment;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -67,28 +70,41 @@ public class StationsFragment extends Fragment {
 
         LocationManager locationManager = (LocationManager) getActivity()
                 .getSystemService(Context.LOCATION_SERVICE);
-        gps = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        LocationListener locationListener = new LocationListener() {
-            public void onStatusChanged(String provider, int status,
-                                        Bundle extras) {
-            }
 
-            public void onProviderEnabled(String provider) {
-            }
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+        } else {
 
-            public void onProviderDisabled(String provider) {
-            }
+            gps = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-            @Override
-            public void onLocationChanged(Location location) {
-                if (location != null) {
-                    gps = location;
+            LocationListener locationListener = new LocationListener() {
+                public void onStatusChanged(String provider, int status,
+                                            Bundle extras) {
                 }
-            }
-        };
-        locationManager.requestLocationUpdates(
-                LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+                public void onProviderEnabled(String provider) {
+                }
+
+                public void onProviderDisabled(String provider) {
+                }
+
+                @Override
+                public void onLocationChanged(Location location) {
+                    if (location != null) {
+                        gps = location;
+                    }
+                }
+            };
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }
 
         return v;
     }
